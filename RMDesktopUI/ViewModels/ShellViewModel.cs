cd9198442b32afae7df477e3b011d1c533d3bde2
Henrique Souza <martins_henrique@uninove.edu.br>
2020-05-04 22:52:35 -0300

    Implements event aggregator - Subscribing to an LogOnEvent
    Add code to the constructor in order to subscribe the ShellViewModel class to handle events of type LogonEvent.
    What we programmed to happen is to activate a new Screen into the Conductor, in this case the SalesVM.

--- RMDesktopUI/ViewModels/ShellViewModel.cs
+++ RMDesktopUI/ViewModels/ShellViewModel.cs
@@ -4,20 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Caliburn.Micro;
+using RMDesktopUI.EventModels;
 
 namespace RMDesktopUI.ViewModels
 {
     /// <summary>
     /// Wrapps user-controlls in order to make them visible without oppening to many windows.
     /// </summary>
-    public class ShellViewModel : Conductor<object>
+    public class ShellViewModel : Conductor<object>, IHandle<LogOnEvent>
     {
-        //Constructor injection
-        private LoginViewModel _loginVM;
-        public ShellViewModel(LoginViewModel loginVM)
+        private IEventAggregator _events;
+        private SalesViewModel _salesVM;
+        private SimpleContainer _container;
+
+        public ShellViewModel(IEventAggregator events, SalesViewModel salesVM,
+            SimpleContainer container)
         {
-            _loginVM = loginVM;
-            ActivateItem(_loginVM);
+            //Help us to publish and subscribe to events.
+            _events = events;
+            _salesVM = salesVM;
+
+            _container = container;
+
+            //Subscribing this class to ever event that should happen in our event system
+            //but we only handle LogOnEvents because our ShellViewModel class listen for 
+            //this particular event messages through the implementation of the interface IHandle<LogOnEvent>
+            _events.Subscribe(this);
+            
+            //Assures that this particular VM (login) is empty ever time it is requested
+            //in order to prevent that sensitive information from some sorting of leaking
+            ActivateItem(_container.GetInstance<LoginViewModel>());
+        }
+               
+        public void Handle(LogOnEvent message)
+        {
+            ActivateItem(_salesVM);          
         }
     }
 }
