ae75f3c12d893365ecd1e82fc403a25c6e3fc32d
Henrique Souza <martins_henrique@uninove.edu.br>
2020-05-04 22:47:26 -0300

    Adds a new Event message in WPF Projet
    We're going to use Event Aggregator pattern to standardize how we handle the flow of events internally, in order to achieve that we added a new message that represents an logon event when its occur.

---
+++ RMDesktopUI/EventModels/LogOnEvent.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMDesktopUI.EventModels
+{
+    /// <summary>
+    /// Representes a Logon event in our system. It serves to be "broadcasted" to every listener of this event
+    /// in order to trigger any special action or expected behavior based on in this event.
+    /// </summary>
+    public class LogOnEvent
+    {
+    }
+}
